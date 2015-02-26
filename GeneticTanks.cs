404a4d1c0dad40f86a76cdcc7054a3416e48cea9
Michael Crawford <mbcrawfo@gmail.com>
2015-02-26 13:42:31 -0500

    Added farseer config

--- GeneticTanks.cs
+++ GeneticTanks.cs
@@ -15,6 +15,11 @@ namespace GeneticTanks
     [STAThread]
     static void Main()
     {
+      // farseer configuration
+      FarseerPhysics.Settings.UseFPECollisionCategories = true;
+      FarseerPhysics.Settings.VelocityIterations = 10;
+      FarseerPhysics.Settings.PositionIterations = 8;
+      
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new Form1());
