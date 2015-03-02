b5850a9f96ae46f566661fdef7fafb8d0c668313
Michael Crawford <mbcrawfo@gmail.com>
2015-03-02 14:32:03 -0500

    Finished EventManager

--- Game/EventManager.cs
+++ Game/EventManager.cs
@@ -1,21 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using GeneticTanks.Game.Events;
 using log4net;
 
 namespace GeneticTanks.Game
 {
+  /// <summary>
+  /// Manages the queueing and dispatching of events in the game.
+  /// </summary>
   sealed class EventManager
   {
     private static readonly ILog Log = LogManager.GetLogger(
       MethodBase.GetCurrentMethod().DeclaringType);
+    
+    /// <summary>
+    /// The base delegate type for all event listeners.
+    /// </summary>
+    /// <param name="evt">
+    /// The arguments for the event.  This is always guaranteed to be of the 
+    /// type that was used when you registered the listener using AddListener.
+    /// </param>
+    public delegate void EventListener(Event evt);
 
-    public delegate void EventListener(EventArgs args);
-
+    #region Private Fields
+    // times event processing
+    private readonly Stopwatch m_eventTimer = new Stopwatch();
+    // maps event types to listeners
     private readonly Dictionary<Type, EventListener> m_eventListeners = 
       new Dictionary<Type, EventListener>();
-    private readonly List<EventArgs> m_pendingEvents = new List<EventArgs>();
+    // acts as a queue for unprocessed events
+    private readonly List<Event> m_pendingEvents = new List<Event>(50);
+    #endregion
+
+    /// <summary>
+    /// Updates the event queue, processing any pending events.
+    /// </summary>
+    /// <param name="maxEventTime">
+    /// The max time in seconds that can spent on event processing.  If 
+    /// processing takes longer than this time, it will be aborted.
+    /// </param>
+    public void Update(float maxEventTime)
+    {
+      if (m_pendingEvents.Count == 0)
+      {
+        return;
+      }
+
+      var count = 0;
+      m_eventTimer.Restart();
+      while (m_pendingEvents.Count > 0)
+      {
+        if (m_eventTimer.Elapsed.TotalSeconds >= maxEventTime)
+        {
+          Log.DebugFormat("Update aborted with {0} events in the queue",
+            m_pendingEvents.Count);
+          break;
+        }
+
+        var evt = m_pendingEvents.First();
+        m_pendingEvents.RemoveAt(0);
+        TriggerEvent(evt);
+        count++;
+      }
+
+      Log.DebugFormat("Processed {0} events in {1:F4} s", count,
+        m_eventTimer.Elapsed.TotalSeconds);
+    }
 
     /// <summary>
     /// Adds a listener for event type T.
@@ -25,7 +78,8 @@ namespace GeneticTanks.Game
     /// <exception cref="ArgumentNullException">
     /// listener is null.
     /// </exception>
-    public void AddListener<T>(EventListener listener) where T : EventArgs
+    public void AddListener<T>(EventListener listener) 
+      where T : Event
     {
       if (listener == null)
       {
@@ -33,8 +87,15 @@ namespace GeneticTanks.Game
       }
 
       var type = typeof (T);
-      m_eventListeners[type] += listener;
-      Log.DebugFormat("Added listener {0} for type {1}", listener, type);
+      if (!m_eventListeners.ContainsKey(type))
+      {
+        m_eventListeners[type] = listener;
+      }
+      else
+      {
+        m_eventListeners[type] += listener;
+      }
+      Log.DebugFormat("Added listener for {0}", type.Name);
     }
 
     /// <summary>
@@ -45,7 +106,8 @@ namespace GeneticTanks.Game
     /// <exception cref="ArgumentNullException">
     /// listener is null.
     /// </exception>
-    public void RemoveListener<T>(EventListener listener) where T : EventArgs
+    public void RemoveListener<T>(EventListener listener)
+      where T : Event
     {
       if (listener == null)
       {
@@ -53,8 +115,11 @@ namespace GeneticTanks.Game
       }
 
       var type = typeof (T);
-      m_eventListeners[type] -= listener;
-      Log.DebugFormat("Removed listener {0} for type {1}", listener, type);
+      if (m_eventListeners.ContainsKey(type))
+      {
+        m_eventListeners[type] -= listener;
+        Log.DebugFormat("Removed listener for {0}", type.Name);
+      }
     }
 
     /// <summary>
@@ -62,7 +127,7 @@ namespace GeneticTanks.Game
     /// </summary>
     /// <param name="evt"></param>
     /// <exception cref="ArgumentNullException">evt is null</exception>
-    public void TriggerEvent(EventArgs evt)
+    public void TriggerEvent(Event evt)
     {
       if (evt == null)
       {
@@ -70,11 +135,15 @@ namespace GeneticTanks.Game
       }
 
       var type = evt.GetType();
-      var listener = m_eventListeners[type];
-      if (listener != null)
+      EventListener listener;
+      if (m_eventListeners.TryGetValue(type, out listener))
       {
-        Log.DebugFormat("Firing event {0}", type);
+        Log.DebugFormat("Firing {0}", type.Name);
         listener(evt);
+      }
+      else
+      {
+        Log.DebugFormat("Discarding {0}, no listeners", type.Name);
       }
     }
 
@@ -83,13 +152,14 @@ namespace GeneticTanks.Game
     /// </summary>
     /// <param name="evt"></param>
     /// <exception cref="ArgumentNullException">evt is null</exception>
-    public void QueueEvent(EventArgs evt)
+    public void QueueEvent(Event evt)
     {
       if (evt == null)
       {
         throw new ArgumentNullException("evt");
       }
       m_pendingEvents.Add(evt);
+      Log.DebugFormat("Queued {0}", evt.GetType().Name);
     }
 
     /// <summary>
@@ -98,15 +168,18 @@ namespace GeneticTanks.Game
     /// <typeparam name="T"></typeparam>
     /// <returns>True if an event was aborted.</returns>
     public bool AbortFirstEvent<T>() 
-      where T : EventArgs
+      where T : Event
     {
       var type = typeof (T);
       var toRemove = m_pendingEvents.First(evt => evt.GetType() == type);
-      if (toRemove != null)
+      if (toRemove == null)
       {
-        m_pendingEvents.Remove(toRemove);
-        Log.DebugFormat("Aborted event of type {0}", type);
+        return false;
       }
+
+      m_pendingEvents.Remove(toRemove);
+      Log.DebugFormat("Aborted event {0}", type.Name);
+      return true;
     }
 
     /// <summary>
@@ -115,11 +188,11 @@ namespace GeneticTanks.Game
     /// <typeparam name="T"></typeparam>
     /// <returns>The number of events removed.</returns>
     public int AbortEvents<T>() 
-      where T : EventArgs
+      where T : Event
     {
       var type = typeof (T);
       var result = m_pendingEvents.RemoveAll(evt => evt.GetType() == type);
-      Log.DebugFormat("Aborted {0} events of type {1}", result, type);
+      Log.DebugFormat("Aborted {0} events of type {1}", result, type.Name);
       return result;
     }
 
---
+++ Game/Events/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GeneticTanks.Game.Events
+{
+  /// <summary>
+  /// The base class for all events in the game.
+  /// </summary>
+  abstract class Event
+    : EventArgs
+  {
+    /// <summary>
+    /// Creates the event.
+    /// </summary>
+    protected Event()
+    {
+      TimeStamp = DateTime.Now.Ticks;
+    }
+
+    /// <summary>
+    /// The creation timestamp of the event in system ticks.
+    /// </summary>
+    public long TimeStamp { get; private set; }
+  }
+}
