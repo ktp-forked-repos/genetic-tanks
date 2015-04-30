8adc8b4be422bda5b4c303d56793119343a814b6
Michael Crawford <mbcrawfo@gmail.com>
2015-04-30 11:24:40 -0400

    Finished TankPopulation

--- GeneticAlgorithm/TankPopulation.cs
+++ GeneticAlgorithm/TankPopulation.cs
@@ -15,16 +15,16 @@ namespace GeneticTanks.GeneticAlgorithm
     private static readonly Random Random = new Random();
     private static readonly int NumClones = Settings.Default.NumClones;
 
-    private static readonly float ReplacementPercent =
-      Settings.Default.ReplacementPercent;
-
-    private static readonly float MutationRate =
-      Settings.Default.MutationRate;
-
     private readonly int m_size;
+    private float m_mutationRate = 0.25f;
+    private float m_replacementPercent = 0.5f;
     private readonly Dictionary<int, TankGenome> m_genomes = 
       new Dictionary<int, TankGenome>();
     
+    /// <summary>
+    /// Create the population in a randomized state.
+    /// </summary>
+    /// <param name="size"></param>
     public TankPopulation(int size)
     {
       if (size <= 0)
@@ -42,24 +42,82 @@ namespace GeneticTanks.GeneticAlgorithm
         m_genomes[i] = genome;
       }
     }
-
+    
+    /// <summary>
+    /// The population size.
+    /// </summary>
     public int Size { get { return m_size; } }
 
+    /// <summary>
+    /// The counter for the current generation.
+    /// </summary>
     public int Generation { get; private set; }
 
+    /// <summary>
+    /// All of the genomes in the current population.
+    /// </summary>
     public IEnumerable<TankGenome> Genomes { get { return m_genomes.Values; } }
 
+    /// <summary>
+    /// Determines how many of the worst performing members of the population 
+    /// will be removed and replaced when a new generation is generated.  Value 
+    /// between 0 and 1.
+    /// </summary>
+    public float ReplacementPercent
+    {
+      get { return m_replacementPercent; }
+      set
+      {
+        value = Math.Min(value, 1f);
+        value = Math.Max(value, 0f);
+        m_replacementPercent = value;
+      }
+    }
+
+    /// <summary>
+    /// The percent chance that a newly generated genome will experience a 
+    /// mutation.  Value between 0 and 1.
+    /// </summary>
+    public float MutationRate
+    {
+      get { return m_mutationRate; }
+      set
+      {
+        value = Math.Min(value, 1f);
+        value = Math.Max(value, 0f);
+        m_mutationRate = value;
+      }
+    }
+
+    /// <summary>
+    /// Get a genome from the population.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>
+    /// The genome, or null if it was not found.
+    /// </returns>
     public TankGenome GetGenome(int id)
     {
       TankGenome result;
       return TryGetGenome(id, out result) ? result : null;
     }
 
+    /// <summary>
+    /// Attempts to get a genome from the population.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="genome"></param>
+    /// <returns>
+    /// True if the genome was retrieved.
+    /// </returns>
     public bool TryGetGenome(int id, out TankGenome genome)
     {
       return m_genomes.TryGetValue(id, out genome);
     }
 
+    /// <summary>
+    /// Generates the next generation of the population.
+    /// </summary>
     public void NextGeneration()
     {
       var avgDamage = Genomes.Average(g => g.DamageDealt);
@@ -113,6 +171,7 @@ namespace GeneticTanks.GeneticAlgorithm
       Generation++;
     }
 
+    // select two random genomes from the list and cross them to make a child
     private static TankGenome DoCrossover(IReadOnlyList<TankGenome> genomes)
     {
       var a = genomes[Random.Next(genomes.Count)];
